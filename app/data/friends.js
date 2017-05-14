9e83e046e8d62752b82c9e8d856257278812fafd
Katie <katiearina@gmail.com>
2017-05-14 15:41:07 -0400

    :love_letter::tongue::hamster: Trying this again..

---
+++ app/data/friends.js
@@ -0,0 +1,42 @@
+// Friend Finder Friends (DATA)
+// =============================================================
+var friends = [{
+	name: "Katie",
+	photoLink: "https://cloud.githubusercontent.com/assets/22947371/26036648/4c8a3618-38af-11e7-960d-da57b2bdddb2.jpg",
+	spaghettiSyrup: 4,
+	treeJump: 5,
+	drawingFly: 5,
+	birthdayBird: 2,
+	jumpsuitFight: 2,
+	kneeElbows: 2,
+	balloonFood: 1,
+	moodTattoo: 5,
+	assassinDollars: 5,
+	clothingBedding: 1
+}, {
+	name: "Cameron",
+	photoLink: "",
+	spaghettiSyrup: "",
+	treeJump: "",
+	drawingFly: "",
+	birthdayBird: "",
+	jumpsuitFight: "",
+	kneeElbows: "",
+	balloonFood: "",
+	moodTattoo: "",
+	assassinDollars: "",
+	clothingBedding: ""
+}, {
+	name: "Mary",
+	photoLink: "",
+	spaghettiSyrup: "",
+	treeJump: "",
+	drawingFly: "",
+	birthdayBird: "",
+	jumpsuitFight: "",
+	kneeElbows: "",
+	balloonFood: "",
+	moodTattoo: "",
+	assassinDollars: "",
+	clothingBedding: ""
+}];
--- server.js
+++ server.js
@@ -18,57 +18,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Friend Finder Friends (DATA)
-// =============================================================
-var friends = [{
-	name: "Katie",
-	photoLink: "https://cloud.githubusercontent.com/assets/22947371/26036648/4c8a3618-38af-11e7-960d-da57b2bdddb2.jpg",
-	spaghettiSyrup: 4,
-	treeJump: 5,
-	drawingFly: 5,
-	birthdayBird: 2,
-	jumpsuitFight: 2,
-	kneeElbows: 2,
-	balloonFood: 1,
-	moodTattoo: 5,
-	assassinDollars: 5,
-	clothingBedding: 1
-}, {
-	name: "Cameron",
-	photoLink: "",
-	spaghettiSyrup: "",
-	treeJump: "",
-	drawingFly: "",
-	birthdayBird: "",
-	jumpsuitFight: "",
-	kneeElbows: "",
-	balloonFood: "",
-	moodTattoo: "",
-	assassinDollars: "",
-	clothingBedding: ""
-}, {
-	name: "Mary",
-	photoLink: "",
-	spaghettiSyrup: "",
-	treeJump: "",
-	drawingFly: "",
-	birthdayBird: "",
-	jumpsuitFight: "",
-	kneeElbows: "",
-	balloonFood: "",
-	moodTattoo: "",
-	assassinDollars: "",
-	clothingBedding: ""
-}];
-
-app.use("/", htmlRoutes.home);
-
 app.get("/survey", htmlRoutes.survey);
 
 app.get("/style.css", function(req, res) {
 	res.sendFile(path.join(__dirname, "app", "public", "style.css"));
 });
 
+app.use("/", htmlRoutes.home);
 
 
 // Starts the server listening

1a63a9e10f9f9feee3c48b0866b31a43b77c3273
Katie <katiearina@gmail.com>
2017-05-15 19:46:15 -0400

    :boot::beers::tada::money_with_wings::coffee::notebook: OMG OMG OMG. Got the friend finder comparison working and it wasn't anywhere near as painful as I thought once I started plugging away at it. More to do (commenting, cleaning up, input validation, etc.), but a great pausing point.

--- app/public/app.js
+++ app/public/app.js
@@ -1,7 +1,7 @@
 // =============================================================
 // Variable Declarations
 
-var newFriend; 
+var newFriend;
 
 // =============================================================
 // Function Declarations
@@ -24,8 +24,8 @@ var clothesBedding = parseInt($("#clothesBedding").val());
 
 	// Grab the survey elements
 	var newFriend = {
-		"name": name,
-		"photo": photo,
+		"name": name.trim(),
+		"photo": photo.trim(),
 		"scores": [
 			spaghettiSyrup, treeJump, drawingFly, birthdayBird,
 			jumpsuitFight, kneeElbows, balloonFood, moodTattoo,
@@ -46,35 +46,57 @@ var clothesBedding = parseInt($("#clothesBedding").val());
 };
 
 function findBestMatch() {
+	var bestMatchFriend;
 	var currentURL = window.location.origin;
 
 	$.ajax({url: currentURL + "/api/friends", method: "GET"})
 		.done(function(friendData){
+			var differenceArray = [];
 	for (var i = 0; i < (friendData.length - 1); i++) {
 		console.log(friendData[i].scores);
+			var difference = 0;
+			for (var j = 0; j < friendData[i].scores.length; j++) {
+				difference += Math.abs((parseInt(friendData[friendData.length - 1].scores[j]) - parseInt(friendData[i].scores[j])));
+				// difference += difference;
+				console.log("Diff: i = " + i + "j = " + j + " " + difference);
+			}
+				differenceArray.push(difference);
+				console.log(differenceArray);
 	}
-		console.log(friendData[friendData.length - 1].scores);
+
+	function getMinOfArray(array) {
+		return Math.min.apply(null, array);
+	};
+		// for (var k = 0; k < differenceArray; k++);
+		// var smallest = Math.min(differenceArray[k]);
+		var smallest = getMinOfArray(differenceArray);
+		bestMatchFriend = differenceArray.indexOf(smallest);
+		console.log(bestMatchFriend);
+
+	$(".modal-body").html("<h1 class='modalname'>" + friendData[bestMatchFriend].name + "</h1>"
+		+ "<img class='modalimage' src='" + friendData[bestMatchFriend].photo + "'>");
+
 	});
 };
 
 // AJAX call to pull and display friendData
-function runFriendQuery() {
-	var currentURL = window.location.origin;
+// function runFriendQuery() {
+// 	var currentURL = window.location.origin;
 
-	$.ajax({url: currentURL + "/api/friends", method: "GET"})
-		.done(function(friendData){
-			console.log("-------------------------------");
-			console.log("URL: " + currentURL + "/api/friends");
-			console.log("-------------------------------");
-			console.log(friendData);
-			console.log("-------------------------------");
+// 	$.ajax({url: currentURL + "/api/friends", method: "GET"})
+// 		.done(function(friendData){
+// 			console.log("-------------------------------");
+// 			console.log("URL: " + currentURL + "/api/friends");
+// 			console.log("-------------------------------");
+// 			console.log(friendData);
+// 			console.log("-------------------------------");
 
-	$(".modal-body").html("<h1 class='modalname'>" + friendData[0].name + "</h1>"
-		+ "<img class='modalimage' src='" + friendData[0].photo + "'>");
+// 	$(".modal-body").html("<h1 class='modalname'>" + friendData[bestMatchFriend].name + "</h1>"
+// 		+ "<img class='modalimage' src='" + friendData[bestMatchFriend].photo + "'>");
 
-		});
+// 		});
 
-};
+// };
 
 // Reset input fields on submit
 function resetSurvey() {

b63a5f635d77c58025bb0f5f50a56145e5c09918
Katie <katiearina@gmail.com>
2017-05-15 15:01:55 -0400

    :globe_with_meridians::sunflower::fallen_leaf::mega: Trying to get modals working with data from friends.js. Not quite there yet.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,17 @@
 // Friend Finder Friends (DATA)
 // =============================================================
-var friendsArray = [{
-	name: "Katie",
-	photoLink: "https://cloud.githubusercontent.com/assets/22947371/26036648/4c8a3618-38af-11e7-960d-da57b2bdddb2.jpg",
-	spaghettiSyrup: 4,
-	treeJump: 1,
-	drawingFly: 5,
-	birthdayBird: 2,
-	jumpsuitFight: 2,
-	kneeElbows: 2,
-	balloonFood: 1,
-	moodTattoo: 5,
-	assassinDollars: 5,
-	clothesBedding: 1
+var friendData = [{
+	"name": "Katie",
+	"photo": "https://cloud.githubusercontent.com/assets/22947371/26036648/4c8a3618-38af-11e7-960d-da57b2bdddb2.jpg",
+	"scores": [4, 1, 5, 2, 2, 2, 1, 5, 5, 1]
 }, {
-	name: "Cameron",
-	photoLink: "https://cloud.githubusercontent.com/assets/22947371/26040324/4ca5a446-38f2-11e7-922d-0b0f139670f8.jpg",
-	spaghettiSyrup: 2,
-	treeJump: 1,
-	drawingFly: 2,
-	birthdayBird: 2,
-	jumpsuitFight: 4,
-	kneeElbows: 3,
-	balloonFood: 1,
-	moodTattoo: 2,
-	assassinDollars: 5,
-	clothesBedding: 1
+	"name": "Cameron",
+	"photo": "https://cloud.githubusercontent.com/assets/22947371/26040324/4ca5a446-38f2-11e7-922d-0b0f139670f8.jpg",
+	"scores": [2, 1, 2, 2, 4, 3, 1, 2, 5, 1]
 }, {
-	name: "Mary",
-	photoLink: "",
-	spaghettiSyrup: 1,
-	treeJump: 1,
-	drawingFly: 5,
-	birthdayBird: 1,
-	jumpsuitFight: 1,
-	kneeElbows: 1,
-	balloonFood: 1,
-	moodTattoo: 2,
-	assassinDollars: 5,
-	clothesBedding: 3
+	"name": "Mary",
+	"photo": "https://cloud.githubusercontent.com/assets/22947371/26072480/a6009cb2-3979-11e7-93de-21f2f1f23877.jpeg",
+	"scores": [1, 1, 5, 1, 1, 1, 1, 2, 5, 3]
 }];
 
-module.exports = friendsArray;
+module.exports = friendData;
--- app/public/app.js
+++ app/public/app.js
@@ -1,27 +1,39 @@
 $("#submit").on("click", function(event) {
 	event.preventDefault();
 
+	var name = $("#name").val().trim();
+	var photo = $("#photoLink").val().trim();
+	var spaghettiSyrup = $("#spaghettiSyrup").val();
+	var treeJump = $("#treeJump").val();
+	var drawingFly = $("#drawingFly").val();
+	var birthdayBird = $("#birthdayBird").val();
+	var jumpsuitFight = $("#jumpsuitFight").val();
+	var kneeElbows = $("#kneeElbows").val();
+	var balloonFood = $("#balloonFood").val();
+	var moodTattoo = $("#moodTattoo").val();
+	var assassinDollars = $("#assassinDollars").val();
+	var clothesBedding = $("#clothesBedding").val(); 
+
 	// Here we grab the survey elements
 	var newFriend = {
-		name: $("#name").val().trim(),
-		photoLink: $("#photoLink").val().trim(),
-		spaghettiSyrup: $("#spaghettiSyrup").val(),
-		treeJump: $("#treeJump").val(),
-		drawingFly: $("#drawingFly").val().trim(),
-		birthdayBird: $("#birthdayBird").val(),
-		jumpsuitFight: $("#jumpsuitFight").val(),
-		kneeElbows: $("#kneeElbows").val(),
-		balloonFood: $("#balloonFood").val(),
-		moodTattoo: $("#moodTattoo").val(),
-		assassinDollars: $("#assassinDollars").val(),
-		clothesBedding: $("#clothesBedding").val(),  
+		"name": name,
+		"photo": photo,
+		"scores": [
+			spaghettiSyrup, treeJump, drawingFly, birthdayBird,
+			jumpsuitFight, kneeElbows, balloonFood, moodTattoo,
+			assassinDollars, clothesBedding
+		] 
 	};
 
 	console.log(newFriend);
 
-$.post("/api/friends", newFriend,
-	function(data) {
-		// alert("API!");
-    });
+	friendData.push(newFriend);
+
+	$.post("/api/friends", newFriend,
+		function(data) {
+			// alert("API!");
+		});
+
+	$(".modal-body").html(newFriend[0].name);
 
 });
